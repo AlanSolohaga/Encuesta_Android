625a4f7a03215b54253b8ac2c2d543ba4a76af2f
AlanSolohaga <64112044+AlanSolohaga@users.noreply.github.com>
2020-07-16 19:30:19 -0300

    YA SE OBTIENEN DATOS DEL USER, SE DESACTIVÓ EL SPLASH DE DESPEDIDA

--- app/src/main/java/com/project/encuesta/MainActivity.java
+++ app/src/main/java/com/project/encuesta/MainActivity.java
@@ -1,5 +1,6 @@
 package com.project.encuesta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -11,19 +12,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.project.encuesta.interfaz.ComunicaFragments;
 import com.project.encuesta.interfaz.PresentInterface;
 import com.project.encuesta.interfaz.VistaInterface;
 import com.project.encuesta.presentador.Presentador;
 import com.project.encuesta.vista.EncuestaFragment;
+import com.project.encuesta.vista.LoginActivity;
 import com.project.encuesta.vista.MenuFragment;
 import com.project.encuesta.vista.SplashDespedida;
 
-public class MainActivity extends AppCompatActivity implements ComunicaFragments {
+public class MainActivity extends AppCompatActivity implements ComunicaFragments, GoogleApiClient.OnConnectionFailedListener {
     FragmentTransaction fragmentTransaction;
     MenuFragment menu;
 
+    private GoogleApiClient googleApiClient;
+
+    int id_user = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +47,14 @@ public class MainActivity extends AppCompatActivity implements ComunicaFragments
         /** INSTANCIO EL FRAGMENT MENU Y LO ASIGNO AL CONTENEDOR DEL MAIN PRINCIPAL**/
         menu = new MenuFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.contenedor,menu).commit();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
 
     @Override
@@ -49,14 +72,77 @@ public class MainActivity extends AppCompatActivity implements ComunicaFragments
     }
 
     @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "ERROR DE CONEC "+connectionResult.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    /**LLAMAMOS AL METODO onStart() para que al iniciar verifiquemos si existe alguna cuenta
+     * registrada**/
+    @Override
+    protected void onStart() {
+        super.onStart();
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+        if(opr.isDone()){
+            GoogleSignInResult result = opr.get();
+            validarResult(result);
+        }else{
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                    validarResult(googleSignInResult);
+                }
+            });
+        }
+    }
+
+    private void validarResult(GoogleSignInResult result) {
+        if(result.isSuccess()){
+            /**Obtenemos el id de la cuenta iniciada*/
+            GoogleSignInAccount account = result.getSignInAccount();
+
+            //Toast.makeText(this, "id_user: "+account.getId(), Toast.LENGTH_SHORT).show();
+            id_user = Integer.parseInt(account.getId());
+            Toast.makeText(this, "Bienvenido/a "+account.getGivenName(), Toast.LENGTH_SHORT).show();
+        }else{
+            /**Caso contrario volvemos a la activity de login**/
+            abrirLogin();
+        }
+    }
+
+    private void abrirLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+
+    /**Metodo para el boton logOut**/
+    public void logOut(View view) {
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if (status.isSuccess()) {
+                    //Toast.makeText(MainActivity.this, "SALGA DE LA APP Y VUELVA A INGRESAR PARA CAMBIAR DE USUARIO", Toast.LENGTH_SHORT).show();
+                    abrirLogin();
+                } else {
+                    Toast.makeText(getApplicationContext(), "NO SE PUDO CERRAR SESIÓN", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    /**CUANDO LA ACTIVIDAD PRINCIPAL SE DESTRUYE INICIO EL SPLASH DESPEDIDA**/
+    /*
+    @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        /**CUANDO LA ACTIVIDAD PRINCIPAL SE DESTRUYE INICIO EL SPLASH DESPEDIDA**/
+
 
         Intent intent = new Intent(MainActivity.this,SplashDespedida.class);
         startActivity(intent);
         finish();
     }
-
+    */
 }
